8e95d374373bb160854e1c4475097a830161144f
bipin-01 <poudel7700@gmail.com>
2022-07-12 12:30:43 +0545

    Project setup

---
+++ src/test/utils.test.ts
@@ -0,0 +1,10 @@
+import { Utils } from '../app/utils'
+
+
+describe('utils test suite', () => {
+    test('first test', () => {
+
+        const abc = Utils.toUpperCase('abc')
+        console.log("First test work")
+    })
+})
