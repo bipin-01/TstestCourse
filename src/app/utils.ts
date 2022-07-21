d3ef65f9339604a10243ebec3febb02a446616b3
bipin-01 <poudel7700@gmail.com>
2022-07-21 12:53:38 +0545

    Learned about expect toBe and toEqual

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,4 +1,13 @@
+import { UrlWithParsedQuery  , parse } from "url";
+
 export class Utils {
+
+    public static parseUrl(url: string): UrlWithParsedQuery {
+
+          return parse(url, true)
+    }
+
+
     public static toUpperCase(arg) {
         return ''
     }
--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -6,5 +6,14 @@ describe('utils test suite', () => {
 
         const abc = Utils.toUpperCase('abc')
         console.log("First test work")
-    })
+    });
+
+    test('parse simple url', ()=> {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+
+        expect(parsedUrl.href).toBe('http://localhost:8080/login')
+        expect (parsedUrl.port).toBe('8080')
+        expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toEqual({})
+    });
 })
