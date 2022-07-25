01d548b0e37bda642e691f7e6bec7e23824f0911
bipin-01 <poudel7700@gmail.com>
2022-07-25 23:36:29 +0545

    Add jest coverage report configuration

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,6 @@ module.exports = {
     testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     verbose: true,
+    collectCoverage: true,
+    collectCoverageFrom: ['<rootDir>/src/app/**/*.ts']
 }
